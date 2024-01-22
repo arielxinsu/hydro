86f00e11baaa129bbbbfa75db10f4971d1269e13
arielxinsu <46763721+arielxinsu@users.noreply.github.com>
2024-01-21 22:17:22 -0800

    update

--- simulation_hydro1_changed.py
+++ simulation_hydro1_changed.py
@@ -578,7 +578,7 @@ s="""
 # == Setup output data write to disk
 dump        dmp_vtk all vtk ${dumpfreq} ./vtk/Particles_*.vtp id type x y z vx vy vz fx fy fz
 dump_modify dmp_vtk pad 8 # ensures filenames file_000000.data
-fix extra all print 6 "Coords of marker atom = $x $y $z" file coord.txt
+fix extra all print 6 "Coords of marker atom = $(x) $(y) $(z)" file coord.txt
 # == simulation time-stepping
 timestep 6
 
